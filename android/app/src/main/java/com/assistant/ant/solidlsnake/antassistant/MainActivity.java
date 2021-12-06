113bb11de6154d334c1e0e1d3cd8d2c5b437a778
Vorkytaka <vorkytaka@gmail.com>
2021-12-07 02:47:24 +0300

    Initial commit

---
+++ android/app/src/main/java/com/assistant/ant/solidlsnake/antassistant/MainActivity.java
@@ -0,0 +1,6 @@
+package com.assistant.ant.solidlsnake.antassistant;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:antassistant/generated/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'ANTAssistant',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        S.delegate,
+      ],
+      home: SizedBox(),
+    );
+  }
+}
