99eaaed046c2549e641b59233c7ac6f987a63365
Vorkytaka <vorkytaka@gmail.com>
2022-02-19 03:42:30 +0300

    • Fix

--- lib/utils/platform/icons.dart
+++ lib/utils/platform/icons.dart
@@ -10,6 +10,15 @@ class PlatformIcons {
       ? CupertinoIcons.profile_circled
       : Icons.account_circle;
 
+  // It's can be useful in case, when we want to check theme's platform
+  // instead of default one
+  //
+  // static IconData getAccountActive([BuildContext? context]) {
+  //   return _isCupertino(context)
+  //       ? CupertinoIcons.profile_circled
+  //       : Icons.account_circle;
+  // }
+
   static IconData get account => defaultTargetPlatform.isCupertino
       ? CupertinoIcons.profile_circled
       : Icons.account_circle_outlined;
@@ -21,4 +30,8 @@ class PlatformIcons {
   static IconData get settings => defaultTargetPlatform.isCupertino
       ? CupertinoIcons.gear
       : Icons.settings_outlined;
+
+  static _isCupertino([BuildContext? context]) => context != null
+      ? Theme.of(context).platform.isCupertino
+      : defaultTargetPlatform.isCupertino;
 }
